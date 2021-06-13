b5ddd5c923e7835a8b5f7eacc8d2351092a081da
cbl <lennart.carbe@gmail.com>
2021-06-13 18:03:23 +0200

    fixed ci

--- bin/format.sh
+++ bin/format.sh
@@ -4,14 +4,12 @@
 # read 'GOOGLE_JAVA_FORMATTER_JAR' from env
 . ./.env
 
-FILES=(`git diff --name-only --cached | grep java`)
-
 java --add-exports jdk.compiler/com.sun.tools.javac.api=ALL-UNNAMED \
     --add-exports jdk.compiler/com.sun.tools.javac.file=ALL-UNNAMED \
     --add-exports jdk.compiler/com.sun.tools.javac.parser=ALL-UNNAMED \
     --add-exports jdk.compiler/com.sun.tools.javac.tree=ALL-UNNAMED \
     --add-exports jdk.compiler/com.sun.tools.javac.util=ALL-UNNAMED \
-    -jar $GOOGLE_JAVA_FORMATTER_JAR --set-exit-if-changed -a -r ${FILES[@]}
+    -jar $GOOGLE_JAVA_FORMATTER_JAR --set-exit-if-changed -a -r $(find . -type f -name "*.java")
 
 retVal=$?
 if [ $retVal -ne 0 ]; then
