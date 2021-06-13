604716dcc7fb9d9c02fc6276ae09c0495b4aa613
herrmanno <O.herrmann92@googlemail.com>
2021-06-13 21:44:40 +0200

    AVLTree implementation (#5)
    - Add AVL tree implementation
    - Add a 'TreePrinter' for visualizing trees
    
    Co-authored-by: Oliver Herrmann <herrmann@id-on.de>
    Co-authored-by: github-actions[bot] <action@github.com>

--- src/main/java/com/github/cbl/algorithm_analyzer/Main.java
+++ src/main/java/com/github/cbl/algorithm_analyzer/Main.java
@@ -1,20 +1,43 @@
 package com.github.cbl.algorithm_analyzer;
 
-import com.github.cbl.algorithm_analyzer.contracts.Algorithm;
+// import com.github.cbl.algorithm_analyzer.contracts.Algorithm;
 import com.github.cbl.algorithm_analyzer.contracts.Event;
 import com.github.cbl.algorithm_analyzer.contracts.EventConsumer;
-import com.github.cbl.algorithm_analyzer.sorts.bubblesort.BubbleSort;
+
+import java.util.Comparator;
+
+// import com.github.cbl.algorithm_analyzer.contracts.Algorithm;
+// import com.github.cbl.algorithm_analyzer.sorts.bubblesort.BubbleSort;
+import com.github.cbl.algorithm_analyzer.trees.AvlTree.AVLTree;
 import com.github.cbl.algorithm_analyzer.util.GeneralEventConsumer;
 import com.github.cbl.algorithm_analyzer.util.LogEventVisitor;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        final Integer[] array = {6, 5, 4, 3, 2, 1};
+        // final Integer[] array = { 6, 5, 4, 3, 2, 1 };
 
-        final Algorithm<Event, BubbleSort.Data<Integer>> a = new BubbleSort<Integer>();
-        final EventConsumer<Event> ec = new GeneralEventConsumer();
+        // final Algorithm<Event,BubbleSort.Data<Integer>> a = new BubbleSort<Integer>();
+        // final EventConsumer<Event> ec = new GeneralEventConsumer();
 
-        a.run(ec, new BubbleSort.Data<>(array));
+        // a.run(ec, new BubbleSort.Data<>(array));
+
+        // ec.visitEvents(new LogEventVisitor());
+
+        AVLTree<Integer> t = new AVLTree<Integer>(Comparator.naturalOrder());
+        EventConsumer<Event> ec = new GeneralEventConsumer();
+        t.onEvent(ec);
+
+        t.insert(4);
+        t.insert(2);
+        t.insert(5);
+        t.insert(1);
+        t.insert(3);
+        t.insert(6);
+        t.insert(10);
+        t.insert(15);
+        t.remove(2);
+        t.insert(13);
+        t.remove(3);
 
         ec.visitEvents(new LogEventVisitor());
     }
--- src/main/java/com/github/cbl/algorithm_analyzer/trees/AvlTree/AvlTree.java
+++
@@ -1,3 +0,0 @@
-package com.github.cbl.algorithm_analyzer.trees.AvlTree;
-
-public class AvlTree {}
