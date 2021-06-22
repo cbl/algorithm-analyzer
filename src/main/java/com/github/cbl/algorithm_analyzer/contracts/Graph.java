403a0c183703cefb76861eb8a1e87307244ce33c
herrmanno <O.herrmann92@googlemail.com>
2021-06-22 12:56:41 +0200

    Add Floyd-Warshall implementation (#19) (#20)
    * Add Floyd-Warshall implementation (#19)
    
    * Apply fixes from CI
    
    Co-authored-by: github-actions[bot] <action@github.com>
    
    Co-authored-by: Oliver Herrmann <o.herrmann92@gmail.com>
    Co-authored-by: github-actions[bot] <action@github.com>

--- src/main/java/com/github/cbl/algorithm_analyzer/contracts/Graph.java
+++ src/main/java/com/github/cbl/algorithm_analyzer/contracts/Graph.java
@@ -1,18 +1,31 @@
 package com.github.cbl.algorithm_analyzer.contracts;
 
+import java.util.Collection;
+import java.util.Optional;
+
 /**
  * @param <V> type of vertices
  * @param <E> type of edge metadata (e.g. weight)
  */
-public interface Graph<V, E> {
+public interface Graph<V, E> extends Cloneable {
 
-    abstract boolean hasEdge(V from, V to);
+    class NoEdgeException extends Exception {}
 
-    abstract int getVerticeCount();
+    boolean hasEdge(V from, V to);
 
-    void setEdge(V from, V to);
+    default E getEdge(V from, V to) throws NoEdgeException {
+        return getEdgeMaybe(from, to).orElseThrow(NoEdgeException::new);
+    }
+
+    Optional<E> getEdgeMaybe(V from, V to);
+
+    int getVerticeCount();
+
+    Collection<V> getVertices();
+
+    void setEdge(V from, V to, E e);
 
     void deleteEdge(V from, V to);
 
-    E getEdges();
+    Graph<V, E> clone();
 }
--- src/main/java/com/github/cbl/algorithm_analyzer/graphs/AdjacentMatrixGraph.java
+++ src/main/java/com/github/cbl/algorithm_analyzer/graphs/AdjacentMatrixGraph.java
@@ -1,12 +1,18 @@
 package com.github.cbl.algorithm_analyzer.graphs;
 
 import com.github.cbl.algorithm_analyzer.contracts.Graph;
+import com.github.cbl.algorithm_analyzer.contracts.WeightFreeGraph;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Optional;
+import java.util.stream.IntStream;
 
 /** Adjacent matrix based graph for int-vertices and no edge weight */
-public class AdjacentMatrixGraph implements Graph<Integer, boolean[][]> {
+public class AdjacentMatrixGraph implements WeightFreeGraph<Integer> {
 
-    private boolean[][] matrix;
-    private int verticeCount;
+    private final boolean[][] matrix;
+    private final int verticeCount;
 
     public AdjacentMatrixGraph(int verticeCount) {
         this.verticeCount = verticeCount;
@@ -16,9 +22,28 @@ public class AdjacentMatrixGraph implements Graph<Integer, boolean[][]> {
         }
     }
 
-    public void setEdge(Integer from, Integer to) {
+    private AdjacentMatrixGraph(int verticeCount, boolean[][] matrix) {
+        this.verticeCount = verticeCount;
+        this.matrix = matrix;
+    }
+
+    @Override
+    public Collection<Integer> getVertices() {
+        return Arrays.asList(
+                IntStream.range(0, verticeCount)
+                        .mapToObj(Integer::valueOf)
+                        .toArray(Integer[]::new));
+    }
+
+    @Override
+    public Optional<Boolean> getEdgeMaybe(Integer from, Integer to) {
+        var e = matrix[from][to];
+        return Optional.ofNullable(e);
+    }
+
+    public void setEdge(Integer from, Integer to, Boolean weight) {
         if (from >= 0 && to >= 0 && from < verticeCount && to < verticeCount) {
-            matrix[from][to] = true;
+            matrix[from][to] = weight;
         }
     }
 
@@ -40,5 +65,14 @@ public class AdjacentMatrixGraph implements Graph<Integer, boolean[][]> {
 
     public boolean[][] getEdges() {
         return matrix;
+    }
+
+    @Override
+    public Graph<Integer, Boolean> clone() {
+        var matrixClone = matrix.clone();
+        for (int i = 0; i < matrix.length; i++) {
+            matrixClone[i] = matrixClone[i].clone();
+        }
+        return new AdjacentMatrixGraph(verticeCount, matrixClone);
     }
 }
