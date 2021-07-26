6d42e5a248d9581db1560c80fa2f9570cd585947
jojomee <jojomee@users.noreply.github.com>
2021-07-26 19:59:58 +0200

    proper handling when element was not found

--- src/main/java/com/github/cbl/algorithm_analyzer/search/BinarySearch.java
+++ src/main/java/com/github/cbl/algorithm_analyzer/search/BinarySearch.java
@@ -9,10 +9,10 @@ import java.util.Arrays;
 import java.util.StringJoiner;
 
 public class BinarySearch<T extends Comparable<T>> implements Algorithm<Event, BinarySearch.Data<T>> {
-    
+
     public static record Data<T extends Comparable<T>>(T[] array, T searchedValue) {}
 
-    public static record FinalStateEvent<T>(T[] array, int foundIndex) implements Event {
+    public static record FinalStateEvent<T>(T[] array, int left, int right, int middle) implements Event {
         @Override
         public String toString() {
             final StringJoiner sj = new StringJoiner("\n");
@@ -20,9 +20,12 @@ public class BinarySearch<T extends Comparable<T>> implements Algorithm<Event, B
             int[] colors = new int[array.length];
             Arrays.fill(colors, -1);
 
-            colors[foundIndex] = 1; // found index -> green
-
-            sj.add(ArrayPrinter.toString(array, colors));
+            if (left <= right) { // search was successful
+                colors[middle] = 1; // found index -> green
+                sj.add(ArrayPrinter.toString(array, colors));
+            } else {
+                sj.add("Element was not found!");
+            }
 
             return sj.toString();
         }
@@ -37,9 +40,15 @@ public class BinarySearch<T extends Comparable<T>> implements Algorithm<Event, B
             int[] colors = new int[array.length];
             Arrays.fill(colors, -1);
 
-            colors[left] = 2; // left and right bounds -> yellow
+            // left and right bounds -> yellow
+            colors[left] = 2;
             colors[right] = 2;
-            colors[middle] = 4; // middle -> purple
+
+            if (left == middle && right == middle) {
+                colors[middle] = 0; // middle -> red
+            } else {
+                colors[middle] = 4; // middle -> purple
+            }
 
             sj.add(ArrayPrinter.toString(array, colors));
 
@@ -49,19 +58,20 @@ public class BinarySearch<T extends Comparable<T>> implements Algorithm<Event, B
 
     public void run(EventConsumer<Event> events, Data<T> data) {
         Arrays.sort(data.array()); // make sure the input array is sorted
-        
+
         binarySearch(events, data.array(), data.searchedValue());
     }
 
     private boolean binarySearch(EventConsumer<Event> events, T[] arr, T searchedValue) {
         int left = 0;
         int right = arr.length - 1;
+        int middle = 0;
 
         while (left <= right) {
-            int middle = (int) Math.floor((left + right) / 2.0);
+            middle = (int) Math.floor((left + right) / 2.0);
 
             if (arr[middle].equals(searchedValue)) {
-                events.accept(new FinalStateEvent<T>(arr, middle));
+                events.accept(new FinalStateEvent<T>(arr, left, right, middle));
 
                 return true;
             }
@@ -74,6 +84,8 @@ public class BinarySearch<T extends Comparable<T>> implements Algorithm<Event, B
                 left = middle + 1;
             }
         }
+
+        events.accept(new FinalStateEvent<T>(arr, left, right, middle));
 
         return false;
     }
